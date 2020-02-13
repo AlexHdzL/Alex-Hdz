7006762b7b03221a3a5041d0d7540ca74681ca10
Alejandro Hdz <alejandrohdzl26@gmail.com>
2020-02-12 19:48:11 -0600

    Subí Postwork01 y Postwork02

---
+++ MiProyecto.1/Sismos-Mexico-1.sh
@@ -0,0 +1,6 @@
+mkdir Proyecto-00-Postwork01 #Se crea proyecto de prueba
+cd Proyecto-00-Postwork01 #Se posiciona sobre proyecto de prueba
+cp -a ../../../Sismos-Mexico.csv . #Se copia del escritorio el archivo descargado
+cp Sismos-Mexico.csv Sismos-Mexico.1.csv #Se hace una copia del archivo a trabajar
+ls -l #Se hace la lista de los archivos
+
